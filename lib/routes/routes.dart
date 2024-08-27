aece44652c1df8c55f7719debb72d315469d571f
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-08-28 04:04:15 +0500

    Update routes.dart

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,11 +4,13 @@ import 'package:flutter_educational_app_ui/screens/sign_in_screen.dart';
 import 'package:flutter_educational_app_ui/screens/sign_up_screen.dart';
 import 'package:flutter_educational_app_ui/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_educational_app_ui/screens/tab_screen.dart';
 
 class PageRoutes {
   Map<String, WidgetBuilder> routes() {
     return {
       SplashScreen.routeName: (context) => const SplashScreen(),
+      TabScreen.routeName: (context) => const TabScreen(),
       SignInScreen.routeName: (context) => const SignInScreen(),
       SignUpScreen.routeName: (context) => const SignUpScreen(),
       ForgotPasswordScreen.routeName: (context) => const ForgotPasswordScreen(),

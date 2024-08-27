631ddc1ca4624057d83c7e923ba0928b0cb6a7e0
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-08-28 04:03:41 +0500

    Update colors.dart

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,50 +1,52 @@
 import 'package:flutter/material.dart';
 
-const Color kPrimaryColor = Color(0xFF212325);
+const Color kPrimaryColor = Color(0xFF746D69);
 MaterialColor kPrimarySwatchColor = MaterialColor(
   kPrimaryColor.value,
   const <int, Color>{
-    50: Color(0xff909192),
-    100: Color(0xff7a7b7c),
-    200: Color(0xff646566),
-    300: Color(0xff4d4f51),
-    400: Color(0xFF37393b),
+    50: Color(0xffbab6b4),
+    100: Color(0xffaca7a5),
+    200: Color(0xff9e9996),
+    300: Color(0xff908a87),
+    400: Color(0xFF827c78),
     500: kPrimaryColor,
-    600: Color(0xff1e2021),
-    700: Color(0xff1a1c1e),
-    800: Color(0xff17191a),
-    900: Color(0xff141516),
-    1000: Color(0xFF111213),
+    600: Color(0xff68625f),
+    700: Color(0xff5d5754),
+    800: Color(0xff514c4a),
+    900: Color(0xff46413f),
+    1000: Color(0xFF3a3735),
   },
 );
-const Color kSecondaryColor = Color(0xFFe1d5c9);
+const Color kSecondaryColor = Color(0xFFcccdc6);
 MaterialColor kSecondarySwatchColor = MaterialColor(
   kSecondaryColor.value,
   const <int, Color>{
-    50: Color(0xfff0eae4),
-    100: Color(0xffede6df),
-    200: Color(0xffeae2d9),
-    300: Color(0xffe7ddd4),
-    400: Color(0xFFe4d9ce),
+    50: Color(0xffe6e6e3),
+    100: Color(0xffe0e1dd),
+    200: Color(0xffdbdcd7),
+    300: Color(0xffd6d7d1),
+    400: Color(0xFFd1d2cc),
     500: kSecondaryColor,
-    600: Color(0xffcbc0b5),
-    700: Color(0xffb4aaa1),
-    800: Color(0xff9e958d),
-    900: Color(0xff878079),
-    1000: Color(0xFF716b65),
+    600: Color(0xffb8b9b2),
+    700: Color(0xffa3a49e),
+    800: Color(0xff8f908b),
+    900: Color(0xff7a7b77),
+    1000: Color(0xFF666763),
   },
 );
 
 Color kWhiteColor = const Color(0xFFFFFFFF);
 Color kGreenColor = const Color(0xFF4E937A);
+Color kPurpleColor = const Color(0xFF452c63);
+Color kGreyWhiteColor = const Color(0xFFE8E9EB);
 Color kSkyBlueColor = const Color(0xFF93E1D8);
 Color kLightSkyBlueColor = const Color(0xFFB8F0E8);
 Color kRedColor = const Color(0xFFFF0000);
 Color kGreyColor = Colors.grey[300]!;
-Color kLightGreyColor = const Color(0xFFE2E2E2);
+Color kLightGreyColor = const Color(0xFFACADA8);
 Color kDarkGreyColor = const Color(0xFFCCCCCC);
 Color kLightDarkGreyColor = const Color(0xFFDADADA);
-Color kTextColor = const Color(0xFF909090);
+Color kTextColor = const Color(0xFF262626);
 Color kDarkTextColor = const Color(0xFF3A3A3A);
 Color kBlackColor = const Color(0xFF000000);
 Color kBrownColor = const Color(0xFF5E243F);

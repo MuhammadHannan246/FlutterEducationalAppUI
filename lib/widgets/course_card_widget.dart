85f850830893b26210966ec10ef01efd5109899c
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-08-28 15:32:45 +0500

    Update course_card_widget.dart

--- lib/widgets/course_card_widget.dart
+++ lib/widgets/course_card_widget.dart
@@ -1,65 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_educational_app_ui/screens/course_screen.dart';
 import 'package:flutter_educational_app_ui/theme/colors.dart';
 
 class CourseCardWidget extends StatelessWidget {
-  final String courseImage;
-  final String courseTitle;
-  final String courseDescription;
+  final String courseCategoryImage;
+  final String courseCategoryTitle;
+  final String numberOfCourses;
   const CourseCardWidget({
     super.key,
-    required this.courseImage,
-    required this.courseTitle,
-    required this.courseDescription,
+    required this.courseCategoryImage,
+    required this.courseCategoryTitle,
+    required this.numberOfCourses,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-      decoration: BoxDecoration(
-        color: kGreyWhiteColor,
-        boxShadow: [
-          BoxShadow(
-            color: kLightGreyColor,
-            blurRadius: 5,
-            offset: const Offset(0, 2),
-          )
-        ],
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius:
-                BorderRadius.circular(6.0), 
-            child: Image.asset(
-              courseImage,
-              width: double.infinity,
-              // height: 150,
-              fit: BoxFit.cover, 
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed(CourseScreen.routeName);
+
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        decoration: BoxDecoration(
+          color: kGreyWhiteColor,
+          boxShadow: [
+            BoxShadow(
+              color: kLightGreyColor,
+              blurRadius: 5,
+              offset: const Offset(0, 2),
+            )
+          ],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6.0),
+              child: Image.asset(
+                courseCategoryImage,
+                width: double.infinity,
+                // height: 100,
+                fit: BoxFit.contain,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              courseTitle,
-              style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                    color: kTextColor,
-                  ),
-              overflow: TextOverflow.ellipsis,
-              maxLines: 1,
+            const SizedBox(height: 16.0),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                courseCategoryTitle,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: kTextColor,
+                    ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
             ),
-          ),
-          Text(
-            courseDescription,
-            style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                  color: kTextColor,
-                ),
-            overflow: TextOverflow.ellipsis,
-            maxLines: 1,
-          ),
-        ],
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                numberOfCourses,
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                      color: kTextColor.withOpacity(0.6),
+                      fontWeight: FontWeight.bold,
+                    ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
